1a4ad8ae147c006b4ce6c09ad5af7228180a7fa2
danstevemukulupi <danstevemukulupi15@hotmail.com>
2026-07-29 22:16:37 +0100

    videos done

--- devsphere-studio/components/sections/Mapovote.tsx
+++ devsphere-studio/components/sections/Mapovote.tsx
@@ -16,7 +16,7 @@ export default function Mapovote() {
           controls
           className="w-4/5 max-5xl rounded-xl shadow-xl"
         >
-          <source src="/Mapovote-Finall.mp4" type="video/mp4" />
+          <source src="/Mapovote.mp4" type="video/mp4" />
         </video>
     </div>
    <hr className="border-gray-700 "></hr>
